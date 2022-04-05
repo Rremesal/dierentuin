3637411e99eaf1aad3ef24f120a60745eec9e879
Rremesal <raul.remesal@gmail.com>
2022-04-05 09:46:42 +0200

    fixed het niet-admin menu

--- menu.php
+++ menu.php
@@ -21,7 +21,7 @@
 
 <?php } else {  ?>
     <div class="menu">
-    <a href="logout.php">LOG UIT</a></div>
+    <a href="logout.php" class="logout">LOG UIT</a>
         <a href="home.php">HOME</a>
         <a href="addpage.php">DIEREN TOEVOEGEN</a>
         <a href="overview.php">OVERZICHT</a>
