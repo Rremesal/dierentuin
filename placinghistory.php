6baf1173da52ace05cc4de72ae9457eddc5ef6d6
Rremesal <raul.remesal@gmail.com>
2022-04-06 09:26:45 +0200

    filter op datum toegevoegd

--- placinghistory.php
+++ placinghistory.php
@@ -8,7 +8,7 @@
 <body>
     <?php require("menu.php");?>
     <h1 class="header">Geschiedenis Plaatsing</h1>
-    <div id="toolsDiv">
+    <div class="toolsDiv">
         <form method="POST">
         <input type="radio" name="radioDate"/> Laatste datum eerst <br/>
         <br/>
@@ -37,9 +37,9 @@
 
     <?php 
         if(isset($_POST['btnSubmit'])) {
-            if(isset($_POST['searchbar'])) {
+            if(isset($_POST['searchbar']) && isset($_POST['radioDate'])) {
                 $search = $_POST['searchbar'];
-                $searchQuery = "SELECT * FROM history WHERE name LIKE '%$search%' OR area LIKE '%$search%'";
+                $searchQuery = "SELECT * FROM history WHERE name LIKE '%$search%' OR area LIKE '%$search%' ORDER BY date_placed DESC";
                 $stm = $conn->prepare($searchQuery);
                 if($stm->execute()) {
                     while($rows=$stm->fetch(PDO::FETCH_ASSOC)) {
@@ -60,7 +60,44 @@
 
                 
 
-            } 
+            } else if(isset($_POST['searchbar']) && empty($_POST['radioDate'])) {
+                $search = $_POST['searchbar'];
+                $searchQuery = "SELECT * FROM history WHERE name LIKE '%$search%' OR area LIKE '%$search%'";
+                $stm = $conn->prepare($searchQuery);
+                if($stm->execute()) {
+                    while($rows=$stm->fetch(PDO::FETCH_ASSOC)) {
+                        echo "<tr>". 
+                        "<td>".$rows['animal_id']."</td>". 
+                        "<td>".$rows['name']."</td>". 
+                        "<td>".$rows['animal_sort']."</td>". 
+                        "<td>".$rows['behavior']."</td>". 
+                        "<td>".$rows['animalhouse_no']."</td>". 
+                        "<td>".$rows['animalhouse_sort']."</td>". 
+                        "<td>".$rows['area']."</td>". 
+                        "<td>".$rows['date_placed']."</td>". 
+                        "</tr>";
+                        
+                    }
+
+                } 
+            } else {
+                $searchQuery = "SELECT * FROM history ORDER BY date DESC";
+                $stm = $conn->prepare($searchQuery);
+                if($stm->execute()) {
+                    while($rows=$stm->fetch(PDO::FETCH_ASSOC)) {
+                        echo "<tr>". 
+                        "<td>".$rows['animal_id']."</td>". 
+                        "<td>".$rows['name']."</td>". 
+                        "<td>".$rows['animal_sort']."</td>". 
+                        "<td>".$rows['behavior']."</td>". 
+                        "<td>".$rows['animalhouse_no']."</td>". 
+                        "<td>".$rows['animalhouse_sort']."</td>". 
+                        "<td>".$rows['area']."</td>". 
+                        "<td>".$rows['date_placed']."</td>". 
+                        "</tr>";
+                    }
+                }
+            }
         } else {
             $query = "SELECT * FROM history";
                 $stm = $conn->prepare($query);
@@ -80,6 +117,7 @@
                     }
                 } 
         }
+    
 
 
         
